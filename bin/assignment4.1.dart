6542dce6b9eea54fe0528cbdd3ce7b59ddb469ed
Tómas <tomashg1985@gmail.com>
2021-10-17 14:14:23 +0000

    first assignment

---
+++ bin/assignment4.1.dart
@@ -0,0 +1,16 @@
+
+  void main(List<String> arguments) {
+
+  // Assignment 4.1
+  // Write a program to find the sum of all elements of the Listusing a loop.
+
+    List<int> calculate_sum = [13,56,77,3,45,6,944,1,43,7,40,55,67,93,45,754,33];
+
+    num sum = 0;
+    for (num e in calculate_sum) {
+      sum += e;
+
+      print('Sum = $sum');
+
+  }
+    }
